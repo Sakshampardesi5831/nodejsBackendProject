65cec61bcf3f2b5d4813914268b3d89e54403a55
pardesisaksham <saksham.pardesi@zappian.com>
2024-01-21 17:05:40 +0530

    add endpoint of  watchHistory

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -5,6 +5,7 @@ import User from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
 const generateAccessTokenAndRefreshToken = async (userId) => {
   try {
     const user = await User.findById(userId);
@@ -330,12 +331,61 @@ export const getUserChannelProfile = asyncHandler(async (req, res) => {
     },
   ]);
 
-  if(!channel?.length){
+  if (!channel?.length) {
     throw new ApiError(404, "Channel Does not Exist  !!!");
   }
-   return res.status(200).json(new ApiResponse(200, "User Channel Fetch SuccessFully", channel[0]));
+  return res
+    .status(200)
+    .json(new ApiResponse(200, "User Channel Fetch SuccessFully", channel[0]));
 });
 
-
-
-
+export const getWatchHistory = asyncHandler(async (req, res) => {
+  const user = await User.aggregate([
+    {
+      $match: { _id: new mongoose.Types.ObjectId(req.user._id) },
+    },
+    {
+      $lookup: {
+        from: "Video",
+        localField: "watchHistory",
+        foreignField: "_id",
+        as: "watchHistory",
+        pipeline: [
+          {
+            $lookup: {
+              from: "User",
+              localField: "owner",
+              foreignField: "_id",
+              as: "owner",
+              pipeline: [
+                {
+                  $project: {
+                    fullName: 1,
+                    username: 1,
+                    avatar: 1,
+                  },
+                },
+              ],
+            },
+          },
+          {
+            $addFields: {
+              owner: {
+                $first: "$owner",
+              },
+            },
+          },
+        ],
+      },
+    },
+  ]);
+  res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        " Watch History Fetch SuccessFully ",
+        user[0].watchHistory
+      )
+    );
+});
