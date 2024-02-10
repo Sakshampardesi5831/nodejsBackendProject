fcff89f4e7490afc4098846a1e6e02d999c5cf69
pardesisaksham <saksham.pardesi@zappian.com>
2024-02-11 00:50:51 +0530

    uploading file to cloudinary complete

--- src/app.js
+++ src/app.js
@@ -4,16 +4,16 @@ import cors  from 'cors';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
 const app = express();
 
-app.use(cors());
-
-
+app.use(cors({
+    credentials:true
+}));
+app.use(express.static('upload'))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
 app.use(cookieParser());
 
 
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -17,8 +17,8 @@ import { verifyJwt } from "../middlewares/myauth.middleware.js";
 const routes = Router();
 routes.route("/register").post(
   upload.fields([
-    { name: "avatar", maxCount: 1 },
-    { name: "coverimage", maxCount: 1 },
+    { name: "avatar" },
+    { name: "coverimage"},
   ]),
   register
 );
